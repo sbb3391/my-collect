a5a7802ba00dce66c8c51bb0dd6b48fa7d08cc9f
sbb3391 <sbb33391@gmail.com>
2020-11-16 09:57:40 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(collection)
+    i = 0
+    new_array = []
 
+    while i < collection.count
+        new_array.push(yield collection[i])
+        i += 1
+    end
+    new_array
+end
 
